d564e239ae9e43e10fea77301c5c2b52dcc21eea
soham-roy-luminous <czarsoham@gmail.com>
2022-05-17 23:27:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HitChance
+ Calculate attack roll hit chance in D&D 5e for give AC and Hit Bonus
---
+++ main.py
@@ -0,0 +1,47 @@
+def populateD20():  # Creates 20x20 array of [j,i] for state space of 2d20
+    rows, cols = (20, 20)
+    _arr = [[[j, i] for i in range(1, cols + 1)] for j in range(1, rows + 1)]
+    return _arr
+
+def inputData():
+    print("Enter Target AC:")
+    targetAC = int(input())
+    print("Enter Hit Bonus:")
+    hitBonus = int(input())
+    return targetAC, hitBonus
+
+
+def toHitChance(_AC, _bonus, _arr, _mode="reg"):
+    successCount = 0
+    for i in range(len(_arr)):
+        for j in range(len(_arr[0])):
+            if _mode == "reg":    # Case for regular roll
+                roll = _arr[i][j][0]
+            elif _mode == "dis":    # Case for disadvantage
+                if _arr[i][j][0] < _arr[i][j][1]:
+                    roll = _arr[i][j][0]
+                else:
+                    roll = _arr[i][j][1]
+            elif _mode == "adv":    # Case for advantage
+                if _arr[i][j][0] > _arr[i][j][1]:
+                    roll = _arr[i][j][0]
+                else:
+                    roll = _arr[i][j][1]
+
+            if roll >= _AC - _bonus:  # Success condition
+                successCount += 1
+    hitChance = round((successCount / 400) * 100, 2)  # state space = 400
+    if hitChance < 5:
+        hitChance = 5  # Nat 20 always hits, chance for nat 20 is 5%
+    return hitChance
+
+
+if __name__ == '__main__':
+    arr = populateD20()
+    AC, bonus = inputData()
+    attack = toHitChance(AC, bonus, arr)
+    print("Hit percentage: " + str(attack))
+    attackDis = toHitChance(AC, bonus, arr, "dis")
+    print("Hit percentage with disadvantage: " + str(attackDis))
+    attackAdv = toHitChance(AC, bonus, arr, "adv")
+    print("Hit percentage with advantage: " + str(attackAdv))
